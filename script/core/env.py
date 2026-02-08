14b0d618caec472c2f82d563b1f7f7882ebb29f0
Furkan Cabbar <131597994+furkancabbar@users.noreply.github.com>
2026-02-09 00:16:29 +0300

    Fix report records query to be seans-centered

--- script/core/env.py
+++ script/core/env.py
@@ -5,9 +5,18 @@ APP_TITLE = "Leta Takip"
 APP_VERSION = "v2.0"
 APP_BUILD = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
 
-# Personel ücret kuralları (örnek)
+# Personel ücret kuralları (kurum talebine göre)
 PERSONEL_UCRET_KURALLARI = {
+    "Arif Hoca": {"tip": "sabit", "tutar": 2500.0},
+    "Pervin Hoca": {"tip": "yuzde", "oran": 100.0},
     "_default": {"tip": "yuzde", "oran": 40.0},
 }
 
-DEFAULT_THERAPISTS: list[str] = []
+DEFAULT_THERAPISTS: list[str] = [
+    "Pervin Hoca",
+    "Çağlar Hoca",
+    "Elif Hoca",
+    "Arif Hoca",
+    "Sena Hoca",
+    "Aybüke Hoca",
+]
